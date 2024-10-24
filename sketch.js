e71449302510186d6e590b37719ed538dcd44ade
gabriela_titao <gabriela.titao@escola.pr.gov.br>
2024-10-24 08:41:46 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,16 +1,24 @@
-// vingadores-13-ação,aventura,ficção cientifica
-// guardiões da galáxia-12-ação,ficção cientifica
-// ate que as cores acabem-12-drama romance
-// bailarina-18-ação
-// viuva negra-12-ação,ficção cientifica
-//caçadores de demonios-16-drama,ação
-//alquimia das almas-14-romance
-//pretendente surpresa-14-comedia romantica
-//pousando no amor-14-drama,romance,ação
-//sorriso real-12-comedia romantica
+function setup() {
+  createCanvas(400, 400);
+}
 
 function draw() {
-  background("rgb(87,15,28)");
+  background(220);
+}let campoIdade;
+let campoFantasia;
+let campoAventura;
+
+function setup() {
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+  campoAventura = createCheckbox("Gosta de aventura?");
+}
+
+function draw() {
+  background("rgb(200,202,228)");
   let idade = campoIdade.value();
   let gostaDeFantasia = campoFantasia.checked();
   let gostaDeAventura = campoAventura.checked();
@@ -21,113 +29,29 @@ function draw() {
   textSize(38);
   text(recomendacao, width / 2, height / 2);
 }
-function setup() {
-  createCanvas(200, 400);
-}
 
-function draw(pink) {
-  background(220);
-}let campoIdade;
-
-function setup() {
-  createCanvas(400, 400);
-  campoIdade = createInput("15");
-}
-
-function draw() {
-  background(220);
-  let idade = 15; // exemplo de idade
-  let recomendacao = geraRecomendacao(idade);
-  text(recomendacao, width / 2, height / 2);
-}
-
-function geraRecomendacao(idade) {
-  if (idade >= 10) {
-    if (idade >= 14) {
-      return "alquimia das almas";
-    } else {
-      return "caçadores de demonios";
-    }
-  } else {
-    return "pretendente surpresa";
-  }
-}function geraRecomendacao(idade, gostaDeFantasia) {
-    if(idade >= 10) {
-        if(idade >= 14) {
-            return "alquimia das almas";
-        } else {
-            if(gostaDeFantasia){
-                return "caçadores de demonios";
-            } else {
-                return "pretendente surpresa";
-            }
-        }
-    } else {
-        if(gostaDeFantasia) {
-            return "pretendente surpresa";
-        } else {
-            return "sorriso real";
-        }
-    }
-}function setup() {
-    createCanvas(800, 400);
-    createSpan("Sua idade:");
-    campoIdade = createInput("5");
-    campoFantasia = createCheckbox(" Gosta de fantasia?");
-}
-
-function draw() {
-    background("white");
-    let idade = campoIdade.value();
-    let gostaDeFantasia = campoFantasia.checked();
-    let recomendacao = geraRecomendacao(idade, gostaDeFantasia);
-
-    fill(color(76, 0, 115));
-    textAlign(CENTER, CENTER);
-    textSize(38);
-    text(recomendacao, width / 2, height / 2);
-}
-
-function geraRecomendacao(idade, gostaDeFantasia) {
-    if (idade >= 10) {
-        if (idade >= 14) {
-            return "alquimia das almas";
-        } else {
-            if (gostaDeFantasia) {
-                return "caçadores e demonios";
-            } else {
-                return "pousando no amor";
-            }
-        }
-    } else {
-        if (gostaDeFantasia) {
-            return "pousando no amor";
-        } else {
-            return "sorriso real";
-        }
-    }
-}function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
   if (idade >= 10) {
     if (idade >= 18) {
       return "bailarina";
     } else {
       if (idade >= 12) {
         if(gostaDeFantasia || gostaDeAventura) {
-          return "alquimia das almas";          
+          return "caçadores de demonio";          
         } else{
-         return "ate que as cores acabem";
+         return "pretendente surpresa";
         }
       } else {
         if (gostaDeFantasia) {
-          return "caçadores de demonios";
+          return "As aventuras de pi";
         } else {
-          return "pousando no amor";
+          return "Depois da chuva";
         }
       }
     }
   } else {
     if (gostaDeFantasia) {
-      return "pousando no amor";
+      return "alquimia da almas";
     } else {
       return "sorriso real";
     }
